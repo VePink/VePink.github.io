4e18bdec4d4bee7e45480d0131900ed4d89b825d
Vėjas Pinkevičius <vejaspink@gmail.com>
2021-10-27 19:32:16 +0300

    Upgraded icons

--- gallery_populator.js
+++ gallery_populator.js
@@ -1,11 +1,11 @@
 fetch('projects.json')
-    .then(function (response) {
+    .then(function(response) {
         return response.json();
     })
-    .then(function (projectList) {
+    .then(function(projectList) {
         appendData(projectList);
     })
-    .catch(function (err) {
+    .catch(function(err) {
         console.log('error: ' + err);
     });
 
@@ -14,28 +14,28 @@ function appendData(projectList) {
     var projects = document.getElementById("projects");
 
     for (var i = 0; i < projectList.length; i++) {
-        
+
         var card = document.createElement("div");
         card.classList.add("card");
 
-        var onImageClickOpenURL = "https://github.com/VePink/"+ projectList[i].GithubId
+        var onImageClickOpenURL = "https://github.com/VePink/" + projectList[i].GithubId
         if (projectList[i].onImageClickOpenURL != "") {
             onImageClickOpenURL = projectList[i].onImageClickOpenURL
         }
 
-        card.innerHTML = 
-        `
-        <h3 class="card-title">`+ projectList[i].title + ` ` +
+        card.innerHTML =
+            `
+        <h3 class="card-title">` + projectList[i].title + ` ` +
             `<svg>
-                <use xlink:href=`+ projectList[i].projectIcon +`></use>
+                <use xlink:href=` + projectList[i].projectIcon + `></use>
             </svg>
         </h3>
         
-        <li class="tagList"><a href="#">`+ projectList[i].tagList + `</a></li>
+        <li class="tagList"><a href="#">` + projectList[i].tagList + `</a></li>
         
-        <img class="projectImage" src="https://raw.githubusercontent.com/VePink/`+ projectList[i].GithubId +`/main/images/thumbnail.png" 
-            onclick="location.href='`+ onImageClickOpenURL +`';" style="cursor: pointer;">
-        <p class="card-description">`+ projectList[i].description +`</p>
+        <img class="projectImage" src="https://raw.githubusercontent.com/VePink/` + projectList[i].GithubId + `/main/images/thumbnail.png" 
+            onclick="location.href='` + onImageClickOpenURL + `';" style="cursor: pointer;">
+        <p class="card-description">` + projectList[i].description + `</p>
         
         <div id= "sourceCodeResultPlaceholder"></div>
         <div id= "resultToolPlaceholder"></div>
@@ -48,13 +48,12 @@ function appendData(projectList) {
         var sourceCodeResultPlaceholder = document.getElementById("sourceCodeResultPlaceholder");
         if (projectList[i].sourceCodeLanguage == "") {
             sourceCodeResultPlaceholder.remove()
-        }
-        else {
+        } else {
             sourceCodeResultPlaceholder.outerHTML =
-            `<a href="https://github.com/VePink/`+ projectList[i].GithubId +`" target="_blank" class="button">
+                `<a href="https://github.com/VePink/` + projectList[i].GithubId + `" target="_blank" class="button">
                 source
                 <svg>
-                    <use xlink:href="#`+ projectList[i].sourceCodeLanguage +`Icon"></use>
+                    <use xlink:href="#` + projectList[i].sourceCodeLanguage + `_technology_icon"></use>
                 </svg>
             </a>`
         }
@@ -62,13 +61,12 @@ function appendData(projectList) {
         var resultToolPlaceholder = document.getElementById("resultToolPlaceholder");
         if (projectList[i].resultEXEToolName == "") {
             resultToolPlaceholder.remove()
-        }
-        else {
+        } else {
             resultToolPlaceholder.outerHTML =
-            `<a href="https://github.com/VePink/`+ projectList[i].GithubId +`/blob/main/dist/`+ projectList[i].resultEXEToolName+`.exe" target="_blank" class="button"> 
+                `<a href="https://github.com/VePink/` + projectList[i].GithubId + `/blob/main/dist/` + projectList[i].resultEXEToolName + `.exe" target="_blank" class="button"> 
                 tool
                 <svg>
-                    <use xlink:href="#EXEIcon"></use>
+                    <use xlink:href="#EXE_file_icon"></use>
                 </svg>
             </a>`
         }
@@ -76,13 +74,12 @@ function appendData(projectList) {
         var resultDatasetPlaceholder = document.getElementById("resultDatasetPlaceholder");
         if (projectList[i].resultDatasetExtension == "") {
             resultDatasetPlaceholder.remove()
-        }
-        else {
+        } else {
             resultDatasetPlaceholder.outerHTML =
-            `<a href="https://github.com/VePink/`+ projectList[i].GithubId +`/tree/main/Output" target="_blank" class="button">
+                `<a href="https://github.com/VePink/` + projectList[i].GithubId + `/tree/main/Output" target="_blank" class="button">
                 dataset
                 <svg>
-                    <use xlink:href="#`+ projectList[i].resultDatasetExtension +`Icon"></use>
+                    <use xlink:href="#` + projectList[i].resultDatasetExtension + `_file_icon"></use>
                 </svg>
             </a>`
         }
@@ -90,13 +87,12 @@ function appendData(projectList) {
         var resultWebsitePlaceholder = document.getElementById("resultWebsitePlaceholder");
         if (projectList[i].resultWebsiteURL == "") {
             resultWebsitePlaceholder.remove()
-        }
-        else {
+        } else {
             resultWebsitePlaceholder.outerHTML =
-            `<a href="`+ projectList[i].resultWebsiteURL +`" target="_blank" class="button">
+                `<a href="` + projectList[i].resultWebsiteURL + `" target="_blank" class="button">
                 website
                 <svg>
-                    <use xlink:href="#WebsiteIcon"></use>
+                    <use xlink:href="#Website_icon"></use>
                 </svg>
             </a>`
         }
