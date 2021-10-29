d0a271cdbabdb53fc849a8e809c925e09988181a
Vėjas Pinkevičius <vejaspink@gmail.com>
2021-10-29 15:34:15 +0300

    Lighthouse report based optimizations

--- gallery_populator.js
+++ gallery_populator.js
@@ -33,9 +33,10 @@ function appendData(projectList) {
         
         <li class="tagList"><a href="#">` + projectList[i].tagList + `</a></li>
         
-        <img class="projectImage" src="https://raw.githubusercontent.com/VePink/` + projectList[i].GithubId + `/main/images/thumbnail.png" 
+        <img class="projectImage" src="https://raw.githubusercontent.com/VePink/` + projectList[i].GithubId + `/main/images/thumbnail.webp" 
+            alt="` + projectList[i].title + `project thumbnail"
             onclick="location.href='` + onImageClickOpenURL + `';" style="cursor: pointer;">
-        <p class="card-description">` + projectList[i].description + `</p>
+        <p class="card-description" lang="en">` + projectList[i].description + `</p>
         
         <div id= "sourceCodeResultPlaceholder"></div>
         <div id= "resultToolPlaceholder"></div>
@@ -50,7 +51,7 @@ function appendData(projectList) {
             sourceCodeResultPlaceholder.remove()
         } else {
             sourceCodeResultPlaceholder.outerHTML =
-                `<a href="https://github.com/VePink/` + projectList[i].GithubId + `" target="_blank" class="button">
+                `<a href="https://github.com/VePink/` + projectList[i].GithubId + `" target="_blank" class="button" rel="noopener">
                 source
                 <svg>
                     <use xlink:href="#` + projectList[i].sourceCodeLanguage + `_technology_icon"></use>
@@ -63,7 +64,7 @@ function appendData(projectList) {
             resultToolPlaceholder.remove()
         } else {
             resultToolPlaceholder.outerHTML =
-                `<a href="https://github.com/VePink/` + projectList[i].GithubId + `/blob/main/dist/` + projectList[i].resultEXEToolName + `.exe" target="_blank" class="button"> 
+                `<a href="https://github.com/VePink/` + projectList[i].GithubId + `/blob/main/dist/` + projectList[i].resultEXEToolName + `.exe" target="_blank" class="button" rel="noopener"> 
                 tool
                 <svg>
                     <use xlink:href="#EXE_file_icon"></use>
@@ -76,7 +77,7 @@ function appendData(projectList) {
             resultDatasetPlaceholder.remove()
         } else {
             resultDatasetPlaceholder.outerHTML =
-                `<a href="https://github.com/VePink/` + projectList[i].GithubId + `/tree/main/Output" target="_blank" class="button">
+                `<a href="https://github.com/VePink/` + projectList[i].GithubId + `/tree/main/Output" target="_blank" class="button" rel="noopener">
                 dataset
                 <svg>
                     <use xlink:href="#` + projectList[i].resultDatasetExtension + `_file_icon"></use>
@@ -89,7 +90,7 @@ function appendData(projectList) {
             resultWebsitePlaceholder.remove()
         } else {
             resultWebsitePlaceholder.outerHTML =
-                `<a href="` + projectList[i].resultWebsiteURL + `" target="_blank" class="button">
+                `<a href="` + projectList[i].resultWebsiteURL + `" target="_blank" class="button" rel="noopener">
                 website
                 <svg>
                     <use xlink:href="#Website_icon"></use>
